968f52a325d0c744634a96bf5655ea00d30ad6b3
Geovane Vieira <geovane.vieira@gmail.com>
2017-10-11 09:17:12 -0300

    Alterações

--- livro_angular/es6/lib.js
+++ livro_angular/es6/lib.js
@@ -2,3 +2,39 @@
 //Arrow functions
 const somar = (x, y = 1) => x + y;
 console.log(somar(5));
+
+//Destructuring
+let arrNumeros = [1, 2, 3];
+
+const [a, b, c] = arrNumeros;
+console.log(a);
+console.log(b);
+console.log(c);
+
+let meuObjeto = {livro: 'Angular 2', autor: 'Thiago Guedes'};
+const {livro, autor} = meuObjeto;
+console.log(livro);
+console.log(autor); 
+
+//Map
+let arrStrings = ['a', 'b', 'c'];
+arrNumeros = [2, 4, 6];
+novoValor = arrNumeros.map(function (value, index) {
+    console.log(index);
+    console.log(value);
+    return value*2;
+});
+
+//Filter
+let nomes = ['jorge', 'carlos', 'roberto', 'lucas'];
+novoNome = nomes.filter(function(value, index) {
+    return value.includes('a');
+});
+
+console.log(novoNome);
+
+novoNome = nomes.filter((value, index) => value.includes('a'));
+
+console.log(novoNome);
+
+
