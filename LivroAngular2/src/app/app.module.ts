c97e639483fad988019d11b2f2ea352b192de14e
Geovane <geovane.vieira@gmail.com>
2017-10-15 20:55:53 -0200

    Alterações

---
+++ LivroAngular2/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { ListaPessoaComponent } from './lista-pessoa/lista-pessoa.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListaPessoaComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
