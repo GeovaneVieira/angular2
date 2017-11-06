699af282bf71d1f309697f338bbdc1d7fa275ae2
Geovane <geovane.vieira@gmail.com>
2017-11-06 21:30:58 -0200

    Mudanças

--- LivroAngular2/src/app/app.module.ts
+++ LivroAngular2/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ListaPessoaComponent } from './lista-pessoa/lista-pessoa.component';
@@ -10,7 +11,8 @@ import { ListaPessoaComponent } from './lista-pessoa/lista-pessoa.component';
     ListaPessoaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- LivroAngular2/src/app/lista-pessoa/lista-pessoa.component.ts
+++ LivroAngular2/src/app/lista-pessoa/lista-pessoa.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-lista-pessoa',
   templateUrl: './lista-pessoa.component.html',
   styleUrls: ['./lista-pessoa.component.css']
 })
 export class ListaPessoaComponent implements OnInit {
+
+  nome: string = 'Geovane'; 
 
   pessoas: string [];
   
