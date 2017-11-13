14b65e993719731715ff43af2430a24f7d4c65de
Geovane Vieira <geovane.vieira@gmail.com>
2017-11-13 19:16:56 -0200

    Evoluindo

---
+++ LivroAngular2/src/app/ng-if/ng-if.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-if',
+  templateUrl: './ng-if.component.html',
+  styleUrls: ['./ng-if.component.css']
+})
+export class NgIfComponent implements OnInit {
+
+	mostrarNome: boolean;
+	cursos: string [] = [];
+
+
+	mostrar(): void {
+		this.mostrarNome = !this.mostrarNome;
+	}
+
+	getValor(): boolean {
+		return this.mostrarNome
+	}
+
+	addCurso():void {
+		this.cursos.push('Angular 2');
+	}
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
